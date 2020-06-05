af79529e2ba8092bc905d56b484e4ed69d8998c1
vova-upqode <volodymyr.s.upqode@gmail.com>
2020-06-05 16:58:20 +0300

    template fixes

--- themes/connections/header.php
+++ themes/connections/header.php
@@ -16,15 +16,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'connections' ); ?></a>
-
 	<header id="masthead" class="site-header">
 
 		<nav id="site-navigation" class="main-navigation">
