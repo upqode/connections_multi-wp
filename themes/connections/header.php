a30a2cfc2c72f702207f8b8251af64e91ba689d6
MariaUpqode <maria.n.upqode@gmail.com>
2020-08-25 15:21:45 +0300

    hide glossary and search

--- themes/connections/header.php
+++ themes/connections/header.php
@@ -25,10 +25,14 @@ global $is_active_ACF;
 $header_class = '';
 $link = [];
 $back_button = false;
+$hide_glossary = false;
+$search_glossary = false;
 
 if ( $is_active_ACF ) {
 	$link = get_field( 'menu_link', 'option' ) ?: [];
 	$back_button = get_field( 'back_button', 'option' ) ?: false;
+	$hide_glossary = get_field( 'hide_glossary', 'option' ) ?: false;
+	$search_glossary = get_field( 'search_glossary', 'option' ) ?: false;
 	$header_class .= get_field( 'header_position', 'option' ) ?: '';
 }
 
@@ -77,6 +81,7 @@ $referer_page = ( ! empty( $_SERVER['HTTP_REFERER'] ) ) ? $_SERVER['HTTP_REFERER
 			</div>
 
 			<div class="cn-header__help-panel">
+				<?php if ( $search_glossary == false  ) : ?>
 
 				<div class="cn-header__search js-form-toggle-wrapp">
 					<?php get_search_form(); ?>
@@ -84,15 +89,22 @@ $referer_page = ( ! empty( $_SERVER['HTTP_REFERER'] ) ) ? $_SERVER['HTTP_REFERER
 					<i class="fa fa-search"></i>
 				</span>
 				</div>
+				<?php endif;?>
+
+				<?php if ( $hide_glossary == false ) : ?>
+
 				<a href="#" data-dropdown="drop-glossary" class="cn-btn-icon cn-header__help-btn js-dropdown-btn">
 					<i class="fa fa-book" aria-hidden="true"></i>
 				</a>
+				<?php endif;?>
 
 			</div>
+			<?php if ( $hide_glossary == false  ) : ?>
 
 			<div id="drop-glossary" class="cn-header__sidebar js-dropdown ">
 				<?php get_template_part('template-parts/content', 'glossary'); ?>
 			</div>
+			<?php endif;?>
 		</div>
 
 	</header>
